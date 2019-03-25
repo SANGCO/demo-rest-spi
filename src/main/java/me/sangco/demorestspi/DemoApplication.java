ca9620ea9dc90a6a23cca0aea42fc8574896e3cd
SANGCO <sangco@nexybit.com>
2019-03-25 23:51:13 +0900

    Initial commit

---
+++ src/main/java/me/sangco/demorestspi/DemoApplication.java
@@ -0,0 +1,13 @@
+package me.sangco.demorestspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
