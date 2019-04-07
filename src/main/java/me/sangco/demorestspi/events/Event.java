11e84e752f9609d0fd7937a31c7471997892e8e7
SANGCO <sangco@nexybit.com>
2019-04-07 11:37:05 +0900

    Add create events test

---
+++ src/main/java/me/sangco/demorestspi/events/Event.java
@@ -0,0 +1,28 @@
+package me.sangco.demorestspi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id; // 추가 식별자
+    private String name; //이벤트 네임
+    private String description; // 설명
+    private LocalDateTime beginEnrollmentDateTime; //등록 시작일시
+    private LocalDateTime closeEnrollmentDateTime; //종료일시    private LocalDateTime beginEventDateTime; //이벤트 시작일시
+    private LocalDateTime endEventDateTime;   //이벤트 종료일시
+    private String location; // (optional) 이벤트 위치 이게 없으면 온라인 모임
+    private int basePrice; // (optional) 기본 금액
+    private int maxPrice; // (optional) 최고 금액
+    private int limitOfEnrollment; //등록한도
+    private boolean offline; // 오프라인 여부
+    private boolean free; //유료 여부
+    private EventStatus eventStatus; // 이벤트 상태
+}
--- src/test/java/me/sangco/demorestspi/DemoApplicationTests.java
+++ src/test/java/me/sangco/demorestspi/DemoApplicationTests.java
@@ -2,15 +2,31 @@ package me.sangco.demorestspi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class DemoApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    public void contextLoads() {
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isOk());
     }
 
 }
