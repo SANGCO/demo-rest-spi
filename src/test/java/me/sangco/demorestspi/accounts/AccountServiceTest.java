1c5ecf4e34f00b7ae7437759f14cb732729be7f5
SANGCO <sangco@nexybit.com>
2019-04-18 23:24:08 +0900

    예외 테스트

--- src/test/java/me/sangco/demorestspi/accounts/AccountServiceTest.java
+++ src/test/java/me/sangco/demorestspi/accounts/AccountServiceTest.java
@@ -1,23 +1,31 @@
 package me.sangco.demorestspi.accounts;
 
 import me.sangco.demorestspi.common.TestDescription;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -45,4 +53,17 @@ public class AccountServiceTest {
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
+
+    @Test
+    @TestDescription("없는 사용자의 경우 예상되는 예외로 타입과 메세지 확인")
+    public void findByUsernameFail() {
+        // Expected
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
+    }
+
 }
